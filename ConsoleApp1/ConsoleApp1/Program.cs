0412125b993bd086240506ef8c9b16d0867bf80c
Jose Michael Castillo Duarte <jose_castillo5@ucne.edu.do>
2018-11-07 09:42:18 -0400

    Agenda para lenguaje

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Archivo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definición de variables
+            System.String Dijo = "Si";
+            System.String Apellido;
+            System.String Nombre;
+            System.String Celular;
+
+            string fileName = "Agenda.txt";
+            FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+
+            while (Dijo == "Si")
+            {
+                //Carga los datos por teclado
+                Console.Write("Ingrese el Apellido: ");
+                Apellido = Console.ReadLine();
+                Console.Write("Ingrese el Nombre: ");
+                Nombre = Console.ReadLine();
+                Console.Write("Ingrese el Celular: ");
+                Celular = Console.ReadLine();
+
+                //Graba o escribe
+                writer.WriteLine("{0} {1} {2}", Apellido, Nombre, Celular);
+                Console.Write("Desea Ingresar otro (Si/No): ");
+                Dijo = Console.ReadLine();
+            }
+            writer.Close();
+        }
+    }
+}
